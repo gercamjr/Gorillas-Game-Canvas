a3146ff44ce7a98893bb1afbd861fb36253a8b64
Gerardo Camorlinga Jr <gerardo.camorlinga@guardmetrics.com>
2024-05-07 15:27:13 -0500

    finished scaffolding project

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Gorillas the game!
+
+A remake of the classic 1991 Gorillas game built in vanilla HTML, CSS, and JavaScript.
--- index.js
+++ index.js
@@ -10,8 +10,42 @@ const ctx = canvas.getContext('2d')
 newGame()
 
 function newGame() {
-  state = {}
+  //reset game state
+  state = {
+    phase: 'aiming', //aiming | in flight | celebrating
+    currentPlayer: 1,
+    bomb: {
+      x: undefined,
+      y: undefined,
+      rotation: 0,
+      velocity: { x: 0, y: 0 },
+    },
+    // buildings
+    backgroundBuildings: [],
+    buildings: [],
+    blastHoles: [],
+  }
+
+  // generate background buildings
+  for (let i = 0; i < 11; i++) {
+    generateBackgroundBuilding(i)
+  }
+
+  //generate buildings
+  for (let i = 0; i < 8; i++) {
+    generateBuilding(i)
+  }
+
+  initializeBombPosition()
+
+  draw()
 }
+
+function generateBackgroundBuilding() {}
+
+function generateBuilding() {}
+
+function initializeBombPosition() {}
 
 function draw() {}
 
